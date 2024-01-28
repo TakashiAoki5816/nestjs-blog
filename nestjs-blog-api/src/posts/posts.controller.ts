481c34f69ca5cd0ff37c582c7c39ee05d2a07ac7
Takashi Aoki <arfaben_5816@icloud.com>
2024-01-28 20:25:32 +0900

    create:一覧と登録のAPI作成

---
+++ nestjs-blog-api/src/posts/posts.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostType } from './post.interface';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Post()
+  create(@Body() post: PostType): void {
+    this.postsService.create(post);
+  }
+
+}
